fe387f6857b8d2103d6ab497189b3cbdeb32ce9f
BC <barkincavdar@gmail.com>
2022-09-19 09:18:19 -0400

    Edited readme, finished (very x 99) basic iteration of the app and modeling

--- README.md
+++ README.md
@@ -3,23 +3,26 @@
 
 ## What you built? 
 
-TODO: Short description of what you attempted to build
+I came up with an idea to potentially pursue as my project for the class and tried to build some of its components. In basic terms, the app will allow people to lend to and borrow from peers. Lenders will enter the amount of money they want to loan and the interest rate they want to loan at and borrowers will enter a range of amount of money they'd like to borrow. Using mathematical optimization, the app will find the most optimal allocation of these loans to borrowers. At this step, I was able to make a very basic frontend using SwiftUI and modeling of the optimization using CPLEX.
 
-Include some screenshots.
-[How?](https://help.github.com/articles/about-readmes/#relative-links-and-image-paths-in-readme-files)
+Figma Link: https://www.figma.com/file/yfLgd7Y2kLOwQVhwYFqwzp/Untitled?node-id=0%3A1
+![Borrower View](images/borrower_view.png)
+![Lender View](images/lender_view.png)
 
 ## Who Did What?
 
-TODO: who worked on what part?
+I did everything as I worked alone.
 
 ## What you learned
 
-TODO: what worked / what didn't work
+Although I have some experience building apps with SwiftUI, it has been 2 years since I used it. So I had to learn most of the things from scratch. I also learned how to use CPLEX to model optimization problems with its Python API (which isn't pleasant).
 
 ## Authors
 
-TODO: list of authors
+Barkin Cavdaroglu
 
 ## Acknowledgments
 
-TODO: cite any tutorials followed here
+https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
+https://github.com/IBMDecisionOptimization/docplex-examples/blob/18bc95d1c89a268625571c38f93a33f851cdf48f/examples/mp/jupyter/tutorials/Beyond_Linear_Programming.ipynb
+https://www.hackingwithswift.com/quick-start/swiftui
--- loan_matching.py
+++ loan_matching.py
@@ -1,38 +1,86 @@
-from pulp import *
+from docplex.mp.model import Model
 
-path_to_cplex = "/Applications/CPLEX_Studio221/cplex/bin/x86-64_osx/cplex"
-model = LpProblem("Example", LpMinimize)
-solver = CPLEX_CMD(path=path_to_cplex)
-_var = LpVariable('a')
-_var2 = LpVariable('a2')
-model += _var + _var2 == 1
-result = model.solve(solver)
-print(result)
 
-solver_list = listSolvers(onlyAvailable=True)
-print(solver_list)
-# Lender information
-lender_ids = ["L1", "L2", "L3"]
+'''
+Problem formulation
+    Borrower Information
+        k_1 is a vector where each number represents the min.
+        amount a person would like to borrow. 
+        k_2 is a vector where each number represents the max.
+        amount a person would like to borrow.
 
-a = {"L1": 600, "L2": 800, "L3": 200}
+        t_1 is a vector where each number represents the min.
+        amount of time (in weeks) a person would like to borrow
+        for.
+        t_2 is a vector where each number represents the max.
+        amount of time (in weeks) a person would like to borrow
+        for.
 
-b_1 = {"L1": 1.7, "L2": 2.1, "L3": 4.5}
-b_2 = {"L1": 2.5, "L2": 2.9, "L3": 5.2}
+    Lender Information
+        a is a vector where each number represents the amount 
+        a lender is willing to lend.
+        b_1 is a vector where each number represents the min.
+        interest rate a lender is willing to lend at.
+        b_2 is a vector where each number represents the max.
+        interest rate a lender is willing to lend at.
+        b is a vector where each number represents the interest
+        rate a lender has picked to lend at.
+        c_1 is a vector where each number represents the min.
+        amount of time (in weeks) a lender is willing to lend
+        for.
+        c_2 is a vector where each number represents the max.
+        amount of time (in weeks) a lender is willing to lend
+        for.
+'''
 
-b = {"L1": 2.0, "L2": 2.5, "L3": 4.8}
+k_1 = [210]
+k_2 = [250]
+t_1 = [3]
+t_2 = [10]
 
-c_1 = {"L1": 4, "L2": 1, "L3": 1}
-c_2 = {"L1": 10, "L2": 2, "L3": 4}
+a = [600, 800, 200, 300, 125, 540]
+b_1 = [1.7, 2.1, 4.5]
+b_2 = [2.5, 2.9, 5.2]
+b = [0.02, 0.025, 0.048, 0.022, 0.031, 0.047]
+c_1 = [1, 2, 2, 1, 1, 3]
+c_2 = [10, 8, 12, 17, 6, 11]
 
-# Borrower Information
-borrower_ids = ["B1"]
+vars_x = []
+vars_y = []
+indicators = []
 
-k_1 = {"B1": 210}
-k_2 = {"B1": 250}
-k = {"B1": 230}
+model = Model(name='loan_matching')
+model.parameters.optimalitytarget.set(3)
 
-t_1 = {"B1": 3}
-t_2 = {"B1": 6}
-t = {"B1": 4}
+for i in range(len(a)):
+    vars_x.append(model.continuous_var(name='x' + str(i)))
+    vars_y.append(model.integer_var(name='y' + str(i)))
+    indicators.append(model.binary_var(name='ind' + str(i)))
+    
 
+for i, min_borrow_amount in enumerate(k_1):
+    model.add_constraint(model.sum(vars_x[j] * a[j] for j in range(len(vars_x))) >= min_borrow_amount, 'c3_' + str(i))
+
+for i, max_borrow_amount in enumerate(k_2):
+    model.add_constraint(model.sum(vars_x[j] * a[j] for j in range(len(vars_x))) <= max_borrow_amount, 'c4_' + str(i))
+
+model.add_constraint(model.sum(vars_x[i] for i in range(len(vars_x))) == 1, )
+
+for i, min_weeks in enumerate(c_1):
+    model.add_indicator(indicators[i], vars_x[i] >= 0.00001, active_value=1)
+    
+    model.add_if_then(indicators[i] == 1, vars_y[i] >= min_weeks)
+    model.add_if_then(indicators[i] == 1, vars_y[i] <= c_2[i])
+    model.add_if_then(indicators[i] == 1, vars_y[i] <= t_1[0])
+    model.add_if_then(indicators[i] == 1, vars_y[i] >= t_2[0])
+
+
+avg = model.sum(vars_x[i] for i in range(len(vars_x))) / len(vars_x)
+sd = (model.sum_squares((vars_x[i] - avg) for i in range(len(vars_x))) / len(vars_x)) 
+
+model.minimize(model.sum(a[i] * b[i] * vars_x[i] for i in range(len(vars_x))) - sd)
+
+s = model.solve()
+print(s)
+model.print_information()
 
